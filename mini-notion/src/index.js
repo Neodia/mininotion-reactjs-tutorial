10c9ef45f5036826220af7f39be4b44c7db7ef1d
ricardo.nunesdeo <ricardo.nunes-de-oliveira@etu.hesge.ch>
2020-11-14 13:40:40 +0100

    Added multiple text blocs to the editor page.

--- mini-notion/src/index.js
+++ mini-notion/src/index.js
@@ -163,45 +163,64 @@ class Navigation extends React.Component {
 
 }
 class Editor extends React.Component {
+    constructor(props) {
+        super(props);
 
+        // Extract the blocs from the notes later.
+        this.state = {
+            blocs: [ "" ],
+        }
 
-    onEnterPress(event) {
+        this.refToLast = React.createRef();
+        this.jumpTo = null
+
+        this.onEnterPress = this.onEnterPress.bind(this);
+    }
+
+    onEnterPress(event, i) {
         if (event.keyCode === 13) {
-            
+            let newBlocs = this.state.blocs.slice();
+            newBlocs.splice(i + 1, 0, "");
+            this.setState({ blocs: newBlocs });
+
+            this.jumpTo = i + 1;
         }
     }
 
-    renderBlock() {
-        return <EditorBloc onKeyPress={this.onEnterPress} />
+    componentDidUpdate() {
+        if(this.jumpTo) {
+            ReactDOM.findDOMNode(this.refToLast).focus();
+            this.jumpTo = null;
+        }
+    }
+
+    checkChange(event, i) {
+        let blocs = this.state.blocs;
+        blocs[i] = event.target.value;
+        this.setState({ blocs: blocs });
     }
 
     render() {
+        const parentBlocs = this.state.blocs;
+        const blocs = parentBlocs.map((bloc, i) => {
+            return (
+                <EditorBloc value={bloc} onChange={(event) => this.checkChange(event, i)} ref={(inst) => {if(this.jumpTo === i) this.refToLast = inst; }} key={i} onKeyPress={(event) => this.onEnterPress(event, i)} />
+            );
+        });
+        
         return (
             <div className="editor">
-                {this.renderBlock()}
+                {blocs}
             </div>
         );
     }
 
 }
 
-class EditorBloc extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: null
-        }
-
-        this.checkChange = this.checkChange.bind(this);
-    }
-
-    checkChange(event) {
-        this.setState({ text: event.target.value });
-    }
-
+class EditorBloc extends React.Component {    
     render() {
         return (
-            <input type="text" className="editor-bloc" onChange={this.checkChange} onKeyDown={(event) => this.props.onKeyPress(event)}></input>
+            <input type="text" value={this.props.value} className="editor-bloc" onChange={this.props.onChange} onKeyDown={(event) => this.props.onKeyPress(event)}></input>
         );
     }
 }
